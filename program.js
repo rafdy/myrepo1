a06401bb35d474b0b234f1163173a565a1feebba
Rafal Dylewski <kontakt@insponet.pl>
2017-02-03 12:53:04 +0100

    Usunięto komentarze, poprawiono wcięcia, wprowadzono Warinig na LANG === undefined

--- program.js
+++ program.js
@@ -1,7 +1,9 @@
 process.stdin.setEncoding('utf-8');
 process.stdout.write('Write /nodever to see node version, /langsys to see system language and /exit to exit the app\n');
+if (process.env.LANG === undefined) {
+	process.stdout.write('Warning: Please note that system language is not defined in your system enviromental variables!\n');
+}
 process.stdin.on('readable', function() {
-    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
     var input = process.stdin.read();  
     if (input !== null) { 
     	var instruction = input.toString().trim();
@@ -20,14 +22,4 @@ process.stdin.on('readable', function() {
         		process.stderr.write('Wrong instruction!\n');
 		}
 	}
-		
- //   if (input !== null) {
- //       var instruction = input.toString().trim();
- //       if (instruction === '/exit') {
- //           process.stdout.write('Quitting app!\n');
- //           process.exit();
- //       } else {
- //           process.stderr.write('Wrong instruction!\n');
- //       }
- //   }
 });
