823ff75eda12002edce323a19cf01b3b5ab25807
Rafal Dylewski <kontakt@insponet.pl>
2017-02-08 19:19:24 +0100

    Poprawki do zadaniefs.js

--- kodillanpm/zadaniefs.js
+++ kodillanpm/zadaniefs.js
@@ -2,10 +2,16 @@ var fs = require('fs');
 
 var f = function() {
     fs.readdir('./', 'utf-8', function(err, files) {
+    	if (err) throw err;
         fs.writeFile('./zawartosckatalogu.txt', files, function(err) {
-        if (err) throw err;
+        	if (err) throw err;
         });
     });
 };
 
-f();
+try {
+    f()
+}
+catch (err) {
+    console.error(err)
+}
