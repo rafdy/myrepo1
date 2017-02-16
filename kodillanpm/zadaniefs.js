506301be6ba921603ab4d29250afb83e4e2e46c7
Rafal Dylewski <kontakt@insponet.pl>
2017-02-16 21:10:39 +0100

    Poprawki po spotkaniu 16.02

--- kodillanpm/zadaniefs.js
+++ kodillanpm/zadaniefs.js
@@ -2,16 +2,15 @@ var fs = require('fs');
 
 var f = function() {
     fs.readdir('./', 'utf-8', function(err, files) {
-    	if (err) throw err;
+    	if (err) {
+    		console.error(err)
+    	}
         fs.writeFile('./zawartosckatalogu.txt', files, function(err) {
-        	if (err) throw err;
+        	if (err) {
+        		console.error(err)
+        	}
         });
     });
 };
 
-try {
-    f()
-}
-catch (err) {
-    console.error(err)
-}
+f()
