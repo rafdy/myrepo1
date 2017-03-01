f70d7b30f5b413a46b48e3ef3b62f656490d4455
Rafal Dylewski <kontakt@insponet.pl>
2017-03-01 17:15:00 +0100

    Final ToDo App - 4.5 Plugins

--- cwiczeniawebpack/Task4.5_Plugins/src/components/TodoList.js
+++ cwiczeniawebpack/Task4.5_Plugins/src/components/TodoList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import style from './TodoList.css';
-import Todo from './Todo.js'
+import Todo from './Todo.js';
 
 const TodoList = props => {
 	return (
 		<div>
-			{props.tdlist.map(tdelement => <Todo key={tdelement.id} id={tdelement.id} className={style.Todos} tdel={tdelement.text} removeTodo={props.removeTodo}></Todo>)}
+			{props.tdlist.map(tdelement => <Todo key={tdelement.id} id={tdelement.id} className={style.Todos} tdel={tdelement.text} removeTodo={() => props.removeTodo()}></Todo>)}
 		</div>
 	);
 }
--- cwiczeniawebpack/Task4.5_Plugins/src/containers/App.js
+++ cwiczeniawebpack/Task4.5_Plugins/src/containers/App.js
@@ -3,60 +3,42 @@ import uuid from 'uuid';
 import style from './App.css';
 import Title from '../components/Title.js';
 import TodoList from '../components/TodoList.js';
-import TodoFormPresentation from '../components/TodoForm.js';
+import TodoForm from '../components/TodoForm1.js';
 
 class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            data: [{
-                    id: 1,
-                    text: 'clean room'
-                }, 
+            data: [
                 {
-                    id: 2,
-                    text: 'wash the dishes'
-                }, 
-                {
-                    id: 3,
-                    text: 'feed my cat'
-                }],
-            todoValue: ''
+                    text: 'todo1',
+                    id: 1
+                }
+            ]
         };
     }
+    
     addTodo(val){
         const todo = {
             text: val,
             id: uuid.v4(),
         };
         const data = [...this.state.data, todo];
-        this.setState({
-            data,
-            todoValue: ''
-        });
+        this.setState({data});
     }
+    
     removeTodo(id) {
         const remainder = this.state.data.filter(todo => todo.id !== id);
         this.setState({data: remainder});
     }
 
-    onTodoFormPresentationChange(event) {
-        this.setState({
-            todoValue: event.target.value
-        });
-    }
-
     render() {
-    return (
-        <div className={style.TodoApp}>        
-            <Title title='Aplikacja TODO' />
-            <TodoFormPresentation
-                submitForm={value => this.addTodo(value)}
-                todoValue={this.state.todoValue}
-                changeInput={event => this.onTodoFormPresentationChange(event)}
-            />
-            <TodoList tdlist={this.state.data} removeTodo={() => this.removeTodo()} />
-        </div>
+        return (
+            <div className={style.TodoApp}>        
+                <Title title='Aplikacja TODO' />
+                <TodoForm addTodo={() => this.addTodo()} />
+                <TodoList tdlist={this.state.data} removeTodo={() => this.removeTodo()} />
+            </div>
         );
     }
 }
