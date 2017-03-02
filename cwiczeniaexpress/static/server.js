dece88fb955a981883045db5cf5d5a7ebe912d42
Rafal Dylewski <kontakt@insponet.pl>
2017-03-03 00:47:27 +0100

    Serving static files - endpoints

---
+++ cwiczeniaexpress/static/server.js
@@ -0,0 +1,19 @@
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('assets'));
+
+app.get('/', function (req, res) {
+    res.sendFile('/index.html');
+});
+
+app.get('/userform', function (req, res) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.end(JSON.stringify(response));
+});
+
+var server = app.listen(3000);
