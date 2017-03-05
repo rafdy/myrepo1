2b54af2e0baa95960e282e6d933af6a079e9d2a6
Rafal Dylewski <kontakt@insponet.pl>
2017-03-05 15:09:44 +0100

    google auth 5.7

---
+++ cwiczeniaexpress/googleauth/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '132475232541-5lt0olsqic4uo6t6qglprn111qjj0cfl.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': '90cQw8m7KWrcVBskdyw_coYY',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
