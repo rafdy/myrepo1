58c2e79f24b0df81405bb9328c48b3b26164102e
Rafal Dylewski <kontakt@insponet.pl>
2017-03-05 13:39:46 +0100

    html template 5.6

---
+++ cwiczeniaexpress/htmltemplates/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.get('/', function(req, res){
+    res.render('welcomelogin')
+});
+
+app.get('/auth/google', function(req, res){
+    res.render('welcomelogged')
+});
+
+app.listen(3000);
+
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
